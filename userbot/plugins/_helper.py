ff0509e8a7c397fb5e2722e38fc6442b8a9a7e46
eonarmy <60788289+eonarmy@users.noreply.github.com>
2020-02-13 16:55:06 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. Provided by @xditya \n [Check out this dope af channel](t.me/giveaways_24hrs) \n
+            help_string = """Userbot Helper.. Provided by @xhackerkunal \n [JOIN EONARMY](t.me/eonarmy) \n
 `Userbot Helper to reveal all the commands`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
