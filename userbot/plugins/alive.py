9bce99833447b2eb988b03b691b28fa877d8c99c
eonarmy <60788289+eonarmy@users.noreply.github.com>
2020-02-13 16:47:32 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -12,6 +12,6 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xditya\n`Database Status: Databases functioning normally!\n\nI will be always with you, my master!\n`"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xhackerkunal\n`Database Status: Databases functioning normally!\n\nI will be always with you, my master!\n`"
                      f"`My owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/xditya/X-tra-Telegram)")
+                     "[Deploy this userbot Now](https://github.com/eonarmy/userbot)")
